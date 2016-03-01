a3833aacd3340f4d3a2e7b13cbb98ae5128648cf
Connor Shocrylas <Cshocrylas@gmail.com>
2016-02-29 23:10:17 -0500

    File Path Changes

--- js/app.js
+++ js/app.js
@@ -26,6 +26,58 @@ $(window).on('scroll',function(){
     }
 
 
+});
 
+
+// external js: isotope.pkgd.js, classie.js
+
+// ----- getText helper ----- //
+
+var textProp = document.documentElement.textContent !== undefined ? 'textContent' : 'innerText';
+
+function getText( elem ) {
+  return elem[ textProp ];
+}
+
+// -----  ----- //
+
+docReady( function() {
+  // init Isotope
+  var iso = new Isotope( '.grid', {
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
+
+  // bind filter button click
+  var filtersElem = document.querySelector('.filters-link-group');
+  eventie.bind( filtersElem, 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    var filterValue = event.target.getAttribute('data-filter');
+    // use matching filter function
+    filterValue = filterFns[ filterValue ] || filterValue;
+    iso.arrange({ filter: filterValue });
+  });
+
+  // change is-checked class on buttons
+  var buttonGroups = document.querySelectorAll('.filters-group');
+  for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup( buttonGroup );
+  }
 
 });
+
+function radioButtonGroup( buttonGroup ) {
+  eventie.bind( buttonGroup, 'click', function( event ) {
+    // only work with buttons
+    if ( !matchesSelector( event.target, 'button' ) ) {
+      return;
+    }
+    classie.remove( buttonGroup.querySelector('.is-checked'), 'is-checked' );
+    classie.add( event.target, 'is-checked' );
+  });
+}
+
