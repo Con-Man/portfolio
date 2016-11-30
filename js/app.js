ecf9b5dedec338afb324fa3ae9f67ad03d76cd95
Connor Shocrylas <cshocrylas@gmail.com>
2016-11-30 16:06:59 -0500

    Test Changes

--- js/app.js
+++ js/app.js
@@ -48,11 +48,3 @@ docReady(function() {
         })
     })
 });
-
-
-$(document).ready(function() {
-    $('.hover').bind('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
-});
